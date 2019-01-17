8a81fcaf428250fb8bfbcb26c5025c2ef94a8352
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-01-17 08:33:38 -0800

    Dropdown menu in header clickable.

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -6,17 +6,32 @@ import * as actionsCreators from '../../store/actions/index';
 
 class Header extends Component {
 
+  state = {
+    menuActive: false
+  }
+
   onLogout = () => {
     this.props.onLogout( { isAuth: false } );
     this.props.onUpdateFormState( {} );
   }
 
-  render() {
+  onMenuClick = () => {
+    this.setState( state => { 
+      return { menuActive: !state.menuActive } 
+    })
+  }
+
+  render() {    
+    console.log('render Header')
+
+    let menuClasses = ['header__user--menu']
+    if ( this.state.menuActive )
+      menuClasses.push('header__user--menu-active')
 
     let dropdown = (
-      <div className='header__user'>
+      <div className='header__user' onClick={this.onMenuClick}>
         <h4>{this.props.user.username}</h4>
-        <nav className='header__user--menu'>
+        <nav className={menuClasses.join(' ')}>
           <div className='header__user--menu__info'>
             <p className='header__user--menu__info--name'>{this.props.user.name}</p>
             <p className='header__user--menu__info--email'>{this.props.user.email}</p>
