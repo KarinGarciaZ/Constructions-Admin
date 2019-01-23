ebf546dfabded69e5dc0b52f2e8557c7812b4569
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-01-23 08:24:13 -0800

    Added loading spinner to project.

--- src/Components/Layout/Loading.js
+++ src/Components/Layout/Loading.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import spinner from '../../Assets/Images/Project/spinner.png'
 
 const loading = () => {
   return (
-    <div className='loading-container'></div>
+    <div className='loading-container'>
+      <img src={spinner} className='spinner' alt='Loading'/>
+    </div>
   )
 }
 
