428315b6721ae66c0c776ae147d246f9a95413e9
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-01-22 19:01:57 -0800

    baseurl changed.

--- src/axios-connection.js
+++ src/axios-connection.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  //baseURL: 'http://localhost:3001'
-  baseURL: 'https://murmuring-eyrie-84778.herokuapp.com'
+  baseURL: 'http://localhost:3001'
+  //baseURL: 'https://murmuring-eyrie-84778.herokuapp.com'
 })
 
 export default instance;
