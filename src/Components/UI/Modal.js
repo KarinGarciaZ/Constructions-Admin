265e9ec774829db7b0ff23458cc8e3c387e9fc12
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-02-07 18:26:09 -0800

    Modal component created to delete constructions.

---
+++ src/Components/UI/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const modal = (props) => {
+  return (
+    <div className='modal'>
+      <FontAwesomeIcon icon={faTimes} className='modal__close' onClick={props.closeModal}/>
+      <p className='modal__title'>Are you sure you want to delete this Construction?</p>
+      <button className='btn btn-cancel' onClick={props.deleteConst}>Yes</button>
+      <button className='btn btn-edit' onClick={props.closeModal}>No</button>
+    </div>
+  )
+}
+
+export default modal
