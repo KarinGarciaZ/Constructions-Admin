79687417d873c33b846e560f6dc8cc362eba8ee6
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-03-18 18:23:32 -0700

    Created service containers.

---
+++ src/Containers/Services/AllServices.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AllServices extends Component {
+  render() {
+    return (
+      <div>
+        AllServices
+      </div>
+    )
+  }
+}
