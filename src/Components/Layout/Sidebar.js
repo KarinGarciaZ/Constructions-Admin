985c04147f216c69a5bde06c7c221c51e68b9f94
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-03-18 18:26:12 -0700

    Added Service workspace to sidebar.

--- src/Components/Layout/Sidebar.js
+++ src/Components/Layout/Sidebar.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserPlus, faProjectDiagram, faCity, faHome, faPlusSquare, faList } from "@fortawesome/free-solid-svg-icons";
+import { faUserPlus, faProjectDiagram, faCity, faHome, faPlusSquare, faList, faTools } from "@fortawesome/free-solid-svg-icons";
 
 import Navlink from '../UI/Navigation/Navlink';
 
 export default class Sidebar extends Component {
 
   state = {
-    smenuClasses: ['smenu']
+    smenuClassesConstruction: ['smenu'],
+    smenuClassesService: ['smenu']
   } 
 
-  onToggleSubmenu = () => {
-    if ( this.state.smenuClasses.length === 1 )
-      this.setState({smenuClasses: ['smenu', 'smenu-active'] })
+  onToggleSubmenuConstruction = () => {
+    if ( this.state.smenuClassesConstruction.length === 1 )
+      this.setState({smenuClassesConstruction: ['smenu', 'smenu-active'] })
     else
-      this.setState({smenuClasses: ['smenu'] })
+      this.setState({smenuClassesConstruction: ['smenu'] })
+  }
+
+  onToggleSubmenuService = () => {
+    if ( this.state.smenuClassesService.length === 1 )
+      this.setState({smenuClassesService: ['smenu', 'smenu-active'] })
+    else
+      this.setState({smenuClassesService: ['smenu'] })
   }
 
   render() {
-    let classes = [ ...this.state.smenuClasses ]
+    let classesConstruction = [ ...this.state.smenuClassesConstruction ]
+    let classesService = [ ...this.state.smenuClassesService ]
     
     return (
       <div className='sidebar'>
@@ -36,15 +45,33 @@ export default class Sidebar extends Component {
             <FontAwesomeIcon icon={faUserPlus}/>
             <p className='link-name'>Add User</p>
           </Navlink>
+          
+          <li className='navlink' onClick={this.onToggleSubmenuService}>
+            <div className='link'>
+              <FontAwesomeIcon icon={faTools}/>
+              <p className='link-name'>Services</p>
+            </div>            
+          </li>
 
-          <li className='navlink' onClick={this.onToggleSubmenu}>
+          <ul className={classesService.join(' ')}>
+            <Navlink cssClass={'smenu__link'} link='/all-services'>
+              <FontAwesomeIcon icon={faList}/>
+              <p className='link-name'>All Services</p>
+            </Navlink>
+            <Navlink cssClass={'smenu__link'} link='/create-service'>
+              <FontAwesomeIcon icon={faPlusSquare}/>
+              <p className='link-name'>New Service</p>
+            </Navlink>
+          </ul>
+
+          <li className='navlink' onClick={this.onToggleSubmenuConstruction}>
             <div className='link'>
               <FontAwesomeIcon icon={faCity}/>
               <p className='link-name'>Constructions</p>
             </div>            
           </li>
 
-          <ul className={classes.join(' ')}>
+          <ul className={classesConstruction.join(' ')}>
             <Navlink cssClass={'smenu__link'} link='/all-constructions'>
               <FontAwesomeIcon icon={faList}/>
               <p className='link-name'>All Constructions</p>
