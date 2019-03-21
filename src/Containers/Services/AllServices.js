a8bf96eb080d7c5d5b1f6e17c3fd0db247a78ef8
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-03-20 19:52:30 -0700

    All services started and request working

--- src/Containers/Services/AllServices.js
+++ src/Containers/Services/AllServices.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import axios from '../../axios-connection';
 
 export default class AllServices extends Component {
+
+  state = {
+    services: []
+  }
+
+  componentDidMount() {
+    axios.get( '/service' )
+    .then( data => {
+      this.setState({ services: data.data })
+    })
+    .catch( error => console.log(error) )
+  }
+
   render() {
+
+    let services = [ ...this.state.services ];
+    let serviceCards = services.map( service => {
+      return (
+        <div className='services-cards__card'> 
+          <img src={'http://localhost:3001/' + service.image}  alt='ser-img'/>
+        </div>
+      )
+    })
+
     return (
-      <div>
-        AllServices
+      <div className='all-services-container'>
+        <div className='services-cards'> 
+          {serviceCards}
+        </div>
       </div>
     )
   }
