305ccfc38b3d5b2c051dbcc8e481257c19392436
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-03-21 18:00:43 -0700

    Modal text updated.

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -7,7 +7,7 @@ const modal = (props) => {
   return (
     <div className='modal'>
       <FontAwesomeIcon icon={faTimes} className='modal__close' onClick={props.closeModal}/>
-      <p className='modal__title'>Are you sure you want to delete this Construction?</p>
+      <p className='modal__title'>Are you sure you want to delete it? {props.ids}</p>
       <button className='btn btn-cancel' onClick={props.deleteConst}>Yes</button>
       <button className='btn btn-edit' onClick={props.closeModal}>No</button>
     </div>
