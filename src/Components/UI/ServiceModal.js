86e4aaba1e639b27de3f241e5f2bee0d777d9caf
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-04-16 19:09:29 -0500

    Added implementation to see paragraph styles.

--- src/Components/UI/ServiceModal.js
+++ src/Components/UI/ServiceModal.js
@@ -4,12 +4,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const serviceModal = (props) => {
+
+  let paragraphs = null;
+  let service = { ...props.service }
+
+  if( service.description ) {
+    service.description = service.description.split('\n');
+
+    paragraphs = service.description.map( (description, index) => {
+      return(
+        <p className='modal-service--description-p' key={index}>{description}</p>
+      )
+    })
+  }    
+
   return (
     <div className='modal-service'>
       <FontAwesomeIcon icon={faTimes} className='modal__close' onClick={props.closeModal}/>
-      <img src={'http://localhost:3001/' + props.service.image}  alt='ser-img' className='modal-service--img'/>
-      <p className='modal-service--title'><strong>{props.service.name}</strong></p>
-      <p className='modal-service--description'><strong>{props.service.description}</strong></p>
+      <img src={'http://localhost:3001/' + service.image}  alt='ser-img' className='modal-service--img'/>
+      <p className='modal-service--title'><strong>{service.name}</strong></p>
+      <p className='modal-service--description'>{paragraphs}</p>
     </div>
   )
 }
