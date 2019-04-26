c21f7f4a311916bdf3cad685d64cdf715063036c
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-04-26 15:52:44 -0500

    Added with credentials conf.

--- src/axios-connection.js
+++ src/axios-connection.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:3001'  
+  baseURL: 'http://127.0.0.1:3001',
+  withCredentials: true
   //baseURL: 'http://172.16.16.51:3001'
   //baseURL: 'https://murmuring-eyrie-84778.herokuapp.com'
 })
